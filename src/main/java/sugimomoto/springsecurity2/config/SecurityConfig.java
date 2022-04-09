aea2320f6d64c3493104930a9895fa47c2d49e71
sugimomoto <sugimomoto@gmail.com>
2022-04-10 08:55:54 +0900

    Added regarding DB processing

--- src/main/java/sugimomoto/springsecurity2/config/SecurityConfig.java
+++ src/main/java/sugimomoto/springsecurity2/config/SecurityConfig.java
@@ -10,15 +10,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import lombok.RequiredArgsConstructor;
+import sugimomoto.springsecurity2.util.Role;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
+    private final UserDetailsService userDetailsService;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -26,13 +32,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception{
-        web.ignoring().antMatchers("/css/**","/img/**","/webjars/**");
+        web.ignoring().antMatchers("/js/**","/css/**","/img/**","/webjars/**");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/login","/error").permitAll()
+            .antMatchers("/login","/error","/register").permitAll()
+            .antMatchers("/admin/**").hasRole(Role.ADMIN.name())
             .anyRequest().authenticated()
             .and()
         .formLogin()
@@ -47,13 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication()
-            .withUser("admin")
-                .password(passwordEncoder().encode("password"))
-                .authorities("ROLE_ADMIN")
-                .and()
-            .withUser("user")
-                .password(passwordEncoder().encode("password"))
-                .authorities("ROLE_USER");
+        auth.userDetailsService(userDetailsService)
+            .passwordEncoder(passwordEncoder());
     }
 }
--- src/main/java/sugimomoto/springsecurity2/controller/SecurityController.java
+++ src/main/java/sugimomoto/springsecurity2/controller/SecurityController.java
@@ -1,12 +1,26 @@
 package sugimomoto.springsecurity2.controller;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import lombok.RequiredArgsConstructor;
+import sugimomoto.springsecurity2.model.SiteUser;
+import sugimomoto.springsecurity2.repository.SiteUserRepository;
+import sugimomoto.springsecurity2.util.Role;
+
+@RequiredArgsConstructor
 @Controller
 public class SecurityController {
+
+    private final SiteUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     
     @GetMapping("/login")
     public String login(){
@@ -18,6 +32,36 @@ public class SecurityController {
         model.addAttribute("username",loginUser.getName());
         model.addAttribute("role",loginUser.getAuthorities());
         return "user";
+    }
+
+    @GetMapping("/admin/list")
+    public String showAdminList(Model model){
+        model.addAttribute("users", userRepository.findAll());
+        return "list";
+    }
+
+    @GetMapping("/register")
+    public String register(@ModelAttribute("user") SiteUser user){
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String process(@Validated @ModelAttribute("user") SiteUser user, BindingResult result){
+
+        if(result.hasErrors()){
+            return "register";
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        if(user.isAdmin()){
+            user.setRole(Role.ADMIN.name());
+        }else{
+            user.setRole(Role.USER.name());
+        }
+
+        userRepository.save(user);
+
+        return "redirect:/login?register";        
     }
 
 }
