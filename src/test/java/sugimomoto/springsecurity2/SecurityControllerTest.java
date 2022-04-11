f4d9c916df1082837836dc8e8ddd86e180baa7f5
sugimomoto <sugimomoto@gmail.com>
2022-04-12 08:00:25 +0900

    Added tests

---
+++ src/test/java/sugimomoto/springsecurity2/SecurityControllerTest.java
@@ -0,0 +1,17 @@
+package sugimomoto.springsecurity2;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+
+public class SecurityControllerTest {
+    
+}
